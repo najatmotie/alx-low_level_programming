fb4798ee4b82642ada4f456d61ea442c7b1085c3
najatmotie <najatmotie96@gmail.com>
2023-03-27 20:00:33 -0700

    commited

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -5,13 +5,10 @@
  */
 void _puts(char *str)
 {
-	int i;
-
-	i = 0;
-	while (str[i])
+	while (*str != '\0)
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str + 0);
+		str+++;
 	}
 	_putchar('\n');
 }
