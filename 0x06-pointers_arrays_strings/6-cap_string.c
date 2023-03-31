d0410fd8e4fb5213a9891ac5ab1462ec74963158
najatmotie <najatmotie96@gmail.com>
2023-03-30 17:31:07 -0700

    commited

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,10 +12,14 @@ char *cap_string(char *str)
 
 	i = 1;
 	if ((str[0] >= 'a') && (str[0] <= 'z'))
-	str[0] -= 32;
+		str[0] -= 32;
 	while (str[i])
-       	{
-		if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '}' || str[i - 1] == '{') && ((str[i] >= 'a') && (str[i] <= 'z')))
+	{
+		if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '\"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '}' ||
+			str[i - 1] == '{') && (str[i] >= 'a' && str[i] <= 'z'))
 			str[i] -= 32;
 		i++;
 	}
